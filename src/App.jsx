f0e272d90bdbc0835c7b00fcda5fa2b69209825e
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-30 17:15:32 +0300

    used news context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsContextProvider from "./context/NewsContext";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </Router>
+    <NewsContextProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </NewsContextProvider>
   );
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import NewsContainer from "../components/NewsContainer";
+import { NewsContext } from "../context/NewsContext";
 
 const Homepage = () => {
+  const { loading } = useContext(NewsContext);
   return (
     <div className="px-3">
-      <NewsContainer />
+      {loading ? (
+        <h2 className="text-center text-[24px] mt-6">Loading news feed...</h2>
+      ) : (
+        <NewsContainer />
+      )}
     </div>
   );
 };
